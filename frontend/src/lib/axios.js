48fcf3e506811bb0f0a0243fc39ee192f5acfdef
nitish1481 <2022ucp1481@mnit.ac.in>
2025-08-30 13:59:22 +0530

    first commint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:`http://localhost:5000/api`,
+  baseURL:`https://chat-app-th3k.onrender.com/api`,
   withCredentials: true,
   
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL =  "http://localhost:5000/api";
+const BASE_URL =  "https://chat-app-th3k.onrender.com/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
